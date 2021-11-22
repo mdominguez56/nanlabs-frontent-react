dc8a54bb4eac5bdc5ccd9f5dc2f510e9e8ca89f1
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2021-11-22 17:22:16 -0300

    add chakra layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import SidebarWithHeader from "./Components/Menu";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SidebarWithHeader></SidebarWithHeader>
+    </div>
+  );
 }
 
 export default App;
